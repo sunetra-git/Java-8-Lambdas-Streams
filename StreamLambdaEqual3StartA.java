9e519d79a2859c553cb6c86a986374096a253b8f
sunetra-git <60608213+sunetra-git@users.noreply.github.com>
2020-07-22 19:53:18 +0530

    Update StreamLambdaEqual3StartA.java

--- StreamLambdaEqual3StartA.java
+++ StreamLambdaEqual3StartA.java
@@ -12,7 +12,7 @@ public class StreamLambdaEqual3StartA {
 		scanner.nextLine();
 		for(int i=0 ; i < size ; i++){
 			System.out.println("Please enter Strings "+ (1+i));
-            entireList.add(scanner.nextLine());
+                        entireList.add(scanner.nextLine());
 		}
 		List<String> Alist = entireList.stream().filter(x -> x.startsWith("a") && x.length() == 3).collect(Collectors.toList());
 		System.out.println("The Strings which start with letter a and have exactly 3 letters: " + Alist + "\n");
